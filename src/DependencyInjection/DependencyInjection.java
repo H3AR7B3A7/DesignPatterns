6c037082b4dd754c53773d73da4d8541cd4b3901
StevenD'Hondt <steven.dhondt@kenze.be>
2021-03-23 19:24:18 +0100

    :sparkles: Other Mentionables

---
+++ src/DependencyInjection/DependencyInjection.java
@@ -0,0 +1,59 @@
+package DependencyInjection;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class DependencyInjection {
+    private final static SpellChecker sc = new SpellChecker(new DutchDictionary());
+
+    public static void main(String[] args) {
+        System.out.println(sc.isValid("test"));
+        System.out.println(sc.suggestions("tesd"));
+    }
+}
+
+class SpellChecker {
+    private final Lexicon dictionary;
+
+    public SpellChecker(Lexicon dictionary) {
+        this.dictionary = Objects.requireNonNull(dictionary);
+    }
+
+    public boolean isValid(String word){
+        return dictionary.getWords().contains(word);
+    }
+    public List<String> suggestions(String typo){
+        return dictionary.getWords().stream()
+                .filter(word -> word.startsWith(typo.substring(0,typo.length()-1)))
+                .collect(Collectors.toList());
+    }
+}
+
+abstract class Lexicon {
+    private Set<String> words;
+
+    public abstract Set<String> getWords();
+}
+
+class EnglishDictionary extends Lexicon {
+    private final Set<String> words = new TreeSet<>();
+    {words.add("test");}
+
+    @Override
+    public Set<String> getWords() {
+        return new TreeSet<>(words);
+    }
+}
+
+class DutchDictionary extends Lexicon {
+    private final Set<String> words = new TreeSet<>();
+    {words.add("test");}
+
+    @Override
+    public Set<String> getWords() {
+        return new TreeSet<>(words);
+    }
+}
